0e7cd2f7f9cdeb68e0ba509d1b8efe60f198e519
khuong <khuong@inspectorio.com>
2018-11-19 19:26:20 +0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+ARG IMAGE_BASE="inspectorio/docker-java"
+ARG IMAGE_TAG="8"
+
+
+FROM ${IMAGE_BASE}:${IMAGE_TAG}
+MAINTAINER devops@inspectorio.com
+
+ENV PENTAHO_HOME="/pentaho" \
+    DEBIAN_FRONTEND=noninteractive \
+    PENTAHO_CE_VERSION=8.1.0.0-365 \
+    PENTAHO_USER=pentaho \
+    BISERVER_VERSION=8.1 \
+    POSTGRESQL_DRIVER_VERSION=9.4.1212
+
+LABEL server="Pentaho BA Server $PENTAHO_CE_VERSION CE"
+
+COPY . ${PENTAHO_HOME}
+
+RUN echo "Download and unpack Pentaho server..." \
+    && wget --progress=dot:giga https://sourceforge.net/projects/pentaho/files/Pentaho%20${BISERVER_VERSION}/server/pentaho-server-ce-${PENTAHO_CE_VERSION}.zip/download -O pentaho-server-ce-${PENTAHO_CE_VERSION}.zip \
+    && cd ${PENTAHO_HOME} \
+    && unzip -q pentaho-server-ce-${PENTAHO_CE_VERSION}.zip \
+    && rm -f pentaho-server-ce-${PENTAHO_CE_VERSION}.zip \
+    && ln -s $PENTAHO_HOME/pentaho-server /pentaho-server \
+    && apt-get update && apt-get -y install vim \
+    && echo "Add Pentaho user..." \
+    && useradd -md $PENTAHO_HOME -s /bin/bash $PENTAHO_USER \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* 
+    
+#COPY . ${PENTAHO_HOME}
+
+RUN chmod a+x "${PENTAHO_HOME}/entrypoint.sh"
+
+WORKDIR ${PENTAHO_HOME}
+     
+
+EXPOSE 8080
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+: ${BI_JAVA_OPTS:='-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PreserveFramePointer -Djava.awt.headless=true -Dpentaho.karaf.root.transient=true -XX:+HeapDumpOnOutOfMemoryError -XX:ErrorFile=../logs/jvm_error.log -XX:HeapDumpPath=../logs/ -verbose:gc -Xloggc:../logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintStringDeduplicationStatistics -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=64M -XX:OnOutOfMemoryError=/usr/bin/oom_killer -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=utf8 -DDI_HOME=\"$DI_HOME\"'}
+
+JAVA_XMS=${JAVA_XMS:1024}m
+JAVA_XMX=${JAVA_XMS:2048}m
+
+fix_permission() {
+	
+	# all sub-directories
+	find $PENTAHO_HOME -type d -print0 | xargs -0 chown $PENTAHO_USER
+	# and then work directories and files underneath
+	for d in "$PENTAHO_HOME/.pentaho" "$PENTAHO_HOME/data/hsqldb" "$PENTAHO_HOME/biserver-ce/tomcat/logs" \
+		"$PENTAHO_HOME/pentaho-solutions/system/jackrabbit/repository" "$PENTAHO_HOME/tmp"; do
+		[ -d $d ] && chown -Rf $PENTAHO_USER $d/* || true
+	done
+	find $PENTAHO_HOME -type d -print0 -name "*.sh" | xargs -0 chown $PENTAHO_USER
+}
+
+init_biserver() {
+	if [ ! -f $PENTAHO_HOME/.initialized ]; then
+		echo "Initializing BI server..."
+		rm -rf .pentaho/* tmp/* pentaho-solutions/system/jackrabbit/repository/* /tmp/kettle tomcat/temp tomcat/work \
+			&& mkdir -p tmp/kettle tmp/osgi/cache tmp/osgi/data/log tmp/osgi/data/tmp tmp/tomcat/temp tmp/tomcat/work \
+				tomcat/logs/audit pentaho-solutions/system/logs \
+			&& sed -i -e 's|\(CATALINA_OPTS=\)\(.*\)|# http://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source\n  \1" -DKETTLE_HOME='"$KETTLE_HOME $BI_JAVA_OPTS"'"|' /pentaho-server/start-pentaho.sh \
+			&& sed -i -e "s|-Xms4096m -Xmx4096m|-Xms${JAVA_XMS} -Xmx${JAVA_XMX}|" /pentaho-server/pentaho-solutions/system/systemListeners.xml \
+			&& find $PENTAHO_HOME -type d -print0 | xargs -0 chown $PENTAHO_USER \
+			&& touch $PENTAHO_HOME/.initialized			
+	fi
+}
+
+
+
+
+
+# start BI server
+	init_biserver
+	fix_permission
+	
+	# now start the bi server
+	yes | $PENTAHO_HOME/pentaho-server/start-pentaho.sh
+
+exec "$@"
