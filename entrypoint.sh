40b3acd52d64149866523c67d5ad8802aacd645b
khuong <khuong@inspectorio.com>
2018-11-20 18:17:08 +0700

    refactor init_biserver func

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 : ${BI_JAVA_OPTS:='-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PreserveFramePointer -Djava.awt.headless=true -Dpentaho.karaf.root.transient=true -XX:+HeapDumpOnOutOfMemoryError -XX:ErrorFile=../logs/jvm_error.log -XX:HeapDumpPath=../logs/ -verbose:gc -Xloggc:../logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintStringDeduplicationStatistics -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=64M -XX:OnOutOfMemoryError=/usr/bin/oom_killer -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=utf8 -DDI_HOME=\"$DI_HOME\"'}
 
-JAVA_XMS=${JAVA_XMS:2048}m
-JAVA_XMX=${JAVA_XMX:2048}m
-
+JAVA_XMS=${JAVA_XMS:-2048}m
+JAVA_XMX=${JAVA_XMX:-2048}m
+JAVA_MAXPERM=${JAVA_MAXPERM:-512}m
 fix_permission() {
 	
 	# all sub-directories
@@ -25,22 +25,18 @@ init_biserver() {
 			&& mkdir -p tmp/kettle tmp/osgi/cache tmp/osgi/data/log tmp/osgi/data/tmp tmp/tomcat/temp tmp/tomcat/work \
 				tomcat/logs/audit pentaho-solutions/system/logs \
 			&& sed -i -e 's|\(CATALINA_OPTS=\)\(.*\)|# http://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source\n  \1" -DKETTLE_HOME='"$KETTLE_HOME $BI_JAVA_OPTS"'"|' /pentaho-server/start-pentaho.sh \
-			&& sed -i -e "s|-Xms4096m -Xmx4096m|-Xms${JAVA_XMS} -Xmx${JAVA_XMX}|" /pentaho-server/pentaho-solutions/system/systemListeners.xml \
+			&& sed -i -e "s|-Xms4096m -Xmx4096m|-Xms${JAVA_XMS} -Xmx${JAVA_XMX}|" /pentaho-server/start-pentaho.sh \
 			&& find $PENTAHO_HOME -type d -print0 | xargs -0 chown $PENTAHO_USER \
 			&& touch $PENTAHO_HOME/.initialized
 			yes | $PENTAHO_HOME/pentaho-server/start-pentaho.sh		
+	else 
+		# now start the bi server
+		$PENTAHO_HOME/pentaho-server/start-pentaho.sh
 	fi
 }
 
-
-
-
-
 # start BI server
-	init_biserver
-	fix_permission
-	
-	# now start the bi server
-	$PENTAHO_HOME/pentaho-server/start-pentaho.sh
+init_biserver
+fix_permission
 
 exec "$@"
