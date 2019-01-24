3a387f140c5e433a5ec2dc6b67a5c2e1dbeb5dd0
khuong <khuong@inspectorio.com>
2019-01-24 16:38:52 +0700

    remove karaf option in JAVA_OPTS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-: ${BI_JAVA_OPTS:='-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PreserveFramePointer -Djava.awt.headless=true -Dpentaho.karaf.root.transient=true -XX:+HeapDumpOnOutOfMemoryError -XX:ErrorFile=../logs/jvm_error.log -XX:HeapDumpPath=../logs/ -verbose:gc -Xloggc:../logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintStringDeduplicationStatistics -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=64M -XX:OnOutOfMemoryError=/usr/bin/oom_killer -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=utf8 -DDI_HOME=\"$DI_HOME\"'}
+: ${BI_JAVA_OPTS:='-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -Djava.security.egd=file:/dev/./urandom -Xms4096m -Xmx4096m -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PreserveFramePointer -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:ErrorFile=../logs/jvm_error.log -XX:HeapDumpPath=../logs/ -verbose:gc -Xloggc:../logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintHeapAtGC -XX:+PrintStringDeduplicationStatistics -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=2 -XX:GCLogFileSize=64M -XX:OnOutOfMemoryError=/usr/bin/oom_killer -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=utf8 -DDI_HOME=\\"$DI_HOME\\"'}
 
 JAVA_XMS=${JAVA_XMS:-2048}m
 JAVA_XMX=${JAVA_XMX:-2048}m
@@ -24,7 +24,7 @@ init_biserver() {
 		rm -rf .pentaho/* tmp/* pentaho-solutions/system/jackrabbit/repository/* /tmp/kettle tomcat/temp tomcat/work \
 			&& mkdir -p tmp/kettle tmp/osgi/cache tmp/osgi/data/log tmp/osgi/data/tmp tmp/tomcat/temp tmp/tomcat/work \
 				tomcat/logs/audit pentaho-solutions/system/logs \
-			&& sed -i -e "s|\(CATALINA_OPTS=\)\(.*\)|# http://wiki.apache.org/tomcat/HowTo/FasterStartUp#Entropy_Source\n  \1 ${BI_JAVA_OPTS}|" /pentaho-server/start-pentaho.sh \
+			&& sed -i -e "s|\(CATALINA_OPTS=\)\(.*\)|\1\"${BI_JAVA_OPTS}\"|" /pentaho-server/start-pentaho.sh \
 			&& sed -i -e "s|-Xms4096m -Xmx4096m|-Xms${JAVA_XMS} -Xmx${JAVA_XMX}|" /pentaho-server/start-pentaho.sh \
 			&& find $PENTAHO_HOME -type d -print0 | xargs -0 chown $PENTAHO_USER \
 			&& touch $PENTAHO_HOME/.initialized
